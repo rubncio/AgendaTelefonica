950f1857ed378335fc441517a65b6c1803b43b69
RubenCano <erreceeme20@gmail.com>
2026-01-19 12:43:27 +0100

    definiendo esqueleto de proyecto

---
+++ agendaTelefonica.py
@@ -0,0 +1,19 @@
+
+def crearContacto():
+    pass 
+def consultarContacto():
+    pass 
+def buscarContacto():
+    pass
+def consultarFavoritos():
+    pass 
+def consultarBloqueados():
+    pass 
+def modificarContacto():
+    pass 
+def añadirFavorito():
+    pass 
+def bloquearContacto():
+    pass 
+def eliminarContacto():
+    pass 
---
+++ tests/test_agendaTelefonica.py
@@ -0,0 +1,29 @@
+from agendaTelefonica import crearContacto, consultarContacto, consultarFavoritos, consultarBloqueados, modificarContacto, añadirFavorito, bloquearContacto, eliminarContacto, buscarContacto
+import pytest
+#|||crearContacto|||
+#   tests
+def
+#|||consultarContacto|||
+#   tests
+def
+#|||consultarFavoritos|||
+#   tests
+def
+#|||consultarBloqueados|||
+#   tests
+def
+#|||modificarContacto|||
+#   tests
+def
+#|||añadirFavorito|||
+#   tests
+def
+#|||bloquearContacto|||
+#   tests
+def
+#|||eliminarContacto|||
+#   tests
+def
+#|||buscarContacto|||
+#   tests
+def
